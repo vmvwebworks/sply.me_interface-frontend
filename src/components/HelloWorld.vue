e4d885db7c100bd2dd89be5249c5252e7323e839
Marcos <vmvwebworks@gmail.com>
2018-02-19 00:13:21 +0100

    scaffold, wip

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="https://i2.wp.com/www.perusmart.com/wp-content/uploads/2015/07/shut_up_and_take_my_money_by_lolikips-d34yx16.png?fit=700%2C394" alt="Vuetify.js" class="mb-5">
+
+        <!-- <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5"> -->
+        <blockquote>
+          &#8220;Speak and take my money.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;Philip J.Fray</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+blockquote {
+  color: #d2dae2;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import '../node_modules/vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  },
+  theme: {
+    primary: '#d2dae2',
+    secondary: '#2C3A47',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
+    main: '#1e272e',
+    header: '#00d8d6',
+    side: '#485460'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
