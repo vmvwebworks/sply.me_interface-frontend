19cf3fa70b247ad7df393d154b4a338f6cac3674
Marcos <vmvwebworks@gmail.com>
2018-03-11 21:43:12 +0100

    right sidebar following user and search panel

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,9 @@
 
           </v-list-tile-content>
         </v-list-tile>
+
       </v-list>
+
     </v-navigation-drawer>
     <v-toolbar
       height="100px"
@@ -65,11 +67,33 @@
       fixed
       app
     >
+    <v-flex >
 
+        <v-toolbar color="secondary" flat>
+          <v-toolbar-items>
+            <v-text-field prepend-icon="search" solo-inverted flat full-width clearable placeholder="search"></v-text-field>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list two-line>
+          <v-subheader>
+            Following
+          </v-subheader>
+          <v-list-tile avatar v-for="item in items2" :key="item.text" @click="">
+            <v-list-tile-action>
+              <v-icon v-if="item.icon">star</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.text"></v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.avatar}.jpg`" alt="">
+            </v-list-tile-avatar>
+          </v-list-tile>
+        </v-list>
+    </v-flex>
     </v-navigation-drawer>
     <v-footer
       class="footer"
-
       :inset="inset"
       height="120px"
       >
@@ -86,14 +110,29 @@ export default {
       flat: true,
       drawer: false,
       fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
+      items: [
+        {
+        icon: 'person',
+        title: 'Profile settings'
+        },
+        {
+        icon: 'event_available',
+        title: 'Subscription'
+        },
+        {
+        icon: 'payment',
+        title: 'Payment info'
+        }
+      ],
+      items2: [
+          { icon: false, text: 'Jason Oner', avatar: 78 },
+          { text: 'Travis Howard', avatar: 28 },
+          { text: 'Ali Connors', avatar: 38 },
+          { text: 'Cindy Baker', avatar: 48 }
+      ],
       right: true,
       rightDrawer: false,
       title: 'Sply.me'
-
     }
   },
   name: 'App'
